eddb968f0d1f3d18e32e323a2489dc0e7f9fc708
Naganishe <mr.nordakov@gmail.com>
2022-06-06 22:59:06 +0300

    add Quest_2

---
+++ Chapter_1/Quest_2/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine ("Введите номер дня недели: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A == 1)
+
+{
+    Console.WriteLine("Monday");
+}
+
+if (A == 2)
+
+{
+    Console.WriteLine("Tusday");
+}
+
+if (A == 3)
+
+{
+    Console.WriteLine("Wednesday");
+}
+
+if (A == 4)
+
+{
+    Console.WriteLine("Thursday");
+}
+
+if (A == 5)
+
+{
+    Console.WriteLine("Friday");
+}
+
+if (A == 6)
+
+{
+    Console.WriteLine("Saturday");
+}
+
+if (A == 7)
+
+{
+    Console.WriteLine("Sunday");
+}
+
+else
+{
+    Console.WriteLine("Введите корректный номер недели от 1 до 7");
+}
