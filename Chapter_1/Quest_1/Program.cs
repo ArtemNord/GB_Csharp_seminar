a8c9bda82da8708ddb296f0e4a0a243b22fde498
Naganishe <mr.nordakov@gmail.com>
2022-06-07 00:13:51 +0300

    add Quest_1

---
+++ Chapter_1/Quest_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A * A == B)
+
+{
+    Console.WriteLine("YES");
+}
+
+else
+{
+    Console.WriteLine("NO");
+}
