40e524936dad6dbc6b3cf558724165b55c4e6b97
Naganishe <mr.nordakov@mail.ru>
2022-06-09 23:24:18 +0300

    add Chapter2

---
+++ Chapter2/Quest2/Program.cs
@@ -0,0 +1,13 @@
+﻿int N = new Random().Next(100,1000);
+Console.WriteLine("Выпало число: ");
+System.Console.WriteLine(N);
+
+int A = N / 100;
+int A1 = A * 10;
+
+int C = N % 10;
+
+int result = A1 + C;
+
+Console.WriteLine("Result = ");
+Console.Write(result);
---
+++ Chapter2/Quest5/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+
+Console.WriteLine("Для того чтобы узнать является ли одно число квадратом другого, введите их: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+int min = B;
+
+if (max < min)
+{
+    max = B;
+    min = A;
+}
+
+int R = max % min;
+
+if (R == 0)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
